f9b4e6b7b00341ad1cee37341a3654e2742fd5be
MichaelCourter64 <michael.courter.64@gmail.com>
2024-03-05 22:47:37 -0800

    Define ApplicationDbContext
    A simple DbContext for any tables in the database used by the app.
    
    Initially has a table with entries represented by (blog) Category.

---
+++ MySQLThroughEntityFramework/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySQLThroughEntityFramework.Models;
+
+namespace MySQLThroughEntityFramework.Data
+{
+    /// <summary>
+    ///   The database context for the entire app.<para/>
+    ///   <inheritdoc/>
+    /// </summary>
+    /// <inheritdoc/>
+    public class ApplicationDbContext:DbContext
+    {
+        /// <summary>
+        /// Passes along the modified options for running the DbContext.
+        /// </summary>
+        /// <param name="options">mainly supplies the connection string for the db</param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        /// <summary>
+        /// The table representing the categories that blog posts can be associated with.
+        /// </summary>
+        public DbSet<Category> Categories { get; set; }
+    }
+}
