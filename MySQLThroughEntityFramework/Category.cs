c31c93b7cd8bd27ac542ffe6a6fd5d4f17dd8636
MichaelCourter64 <michael.courter.64@gmail.com>
2024-03-05 21:29:06 -0800

    Define Db model Category
    Creates a class representing a blog category based on its name.
    
    Includes Id as a key, DisplayOrder for list sorting, and CreateDate as a timestamp.

---
+++ MySQLThroughEntityFramework/Category.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MySQLThroughEntityFramework.Models
+{
+    /// <summary>
+    /// Blog category data object. Represents an entity in the database.
+    /// </summary>
+    public class Category
+    {
+        /// <summary>
+        /// Key value for storage.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Must not be null or empty when modifiying database state.
+        /// Value must be 2-100 characters in length.
+        /// </summary>
+        [Required(ErrorMessage = "Name field is required.")]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Sorting value for display of this Category.
+        /// </summary>
+        public int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// Defaults to the date it was created at.
+        /// </summary>
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
